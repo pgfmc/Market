4e5aba6838c852cdc5996ff6024bd3875ab841cb
bk <33919755+bkGamingYT@users.noreply.github.com>
2021-03-05 00:49:38 -0600

    Add files via upload

---
+++ src/net/pgfmc/shop/Main.java
@@ -0,0 +1,5 @@
+package net.pgfmc.shop;
+
+public class Main {
+
+}
---
+++ src/net/pgfmc/shop/events/InventoryEvents.java
@@ -0,0 +1,9 @@
+package net.pgfmc.shop.events;
+
+import org.bukkit.event.Listener;
+
+public class InventoryEvents implements Listener {
+	
+	
+
+}
