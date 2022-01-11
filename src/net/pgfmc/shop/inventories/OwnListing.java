e8a0e94e37ae7a52e8781030a59e5d272b5bd527
CrimsonDart <79547580+CrimsonDart@users.noreply.github.com>
2022-01-10 22:35:59 -0600

    kjuiashdgfjad

---
+++ src/net/pgfmc/shop/inventories/OwnListing.java
@@ -0,0 +1,19 @@
+package net.pgfmc.shop.Inventories;
+
+import net.pgfmc.core.inventoryAPI.BaseInventory;
+import net.pgfmc.core.inventoryAPI.extra.SizeData;
+
+public class OwnListing extends BaseInventory {
+    
+    public OwnListing(Listing listing) {
+        super(SizeData.SMALL, "");
+
+        
+
+
+    }
+
+
+
+    
+}
